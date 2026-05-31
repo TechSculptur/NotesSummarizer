deb2364d54a0980ac2c10c66ab154307a049020d
Kushagra Rastogi <150462826+TechSculptur@users.noreply.github.com>
2026-05-31 10:53:51 +0530

    Include NLTK downloads in summarizer.py
    Added NLTK downloads for punkt, stopwords, and wordnet.

--- preprocessing/summarizer.py
+++ preprocessing/summarizer.py
@@ -9,7 +9,11 @@ from collections import Counter
 import math
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import nltk
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 # path=os.path.abspath('data.txt')
 with open('preprocessing\data.txt','r',encoding='UTF-8') as f1:
     txt=f1.read()
